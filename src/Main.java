76751248b8156ea892bfb1353c76ca99d5eb919b
naomi <sarkernr@mail.uc.edu>
2024-09-26 13:22:05 -0400

    Lab 5 Task 1 Complete!

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+       Scanner in = new Scanner(System.in);
+       double itemPrice = 0;
+       double shipCost = 0;
+       double totalCost = 0;
+       final double FREE_SHIP = 100;
+       final double SHIP_RATE = .02;
+       String trash = "";
+
+        System.out.println("Enter the item price: ");
+        if(in.hasNextDouble())
+        {
+
+            itemPrice = in.nextDouble();
+            in.nextLine(); // clear the buffer
+
+            if(itemPrice >= FREE_SHIP)
+            {
+                shipCost = 0;
+                totalCost = itemPrice;
+            }
+
+            else // Have to pay ship costs
+            {
+                shipCost = SHIP_RATE * itemPrice;
+                totalCost = itemPrice + shipCost;
+            }
+            // Display results
+            System.out.println("Shipping costs are: " + shipCost);
+            System.out.println("Total cost are: " + totalCost);
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + "is not a valid number!");
+            System.out.println("Run the program again with correct input!");
+        }
+    }
+}
